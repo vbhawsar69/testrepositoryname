a7d604d15a3263f8620e544c28766b6e4d8bbac6
vivek.bhawsar <vivek.bhawsar@bookmyshow.com>
2015-11-05 12:19:18 +0530

    complete spring core and top tutorial from javatpoint

--- src/main/java/com/gontuseries/springaop/TrackOperation.java
+++ src/main/java/com/gontuseries/springaop/TrackOperation.java
@@ -24,43 +24,42 @@ import org.aspectj.lang.annotation.Pointcut;
 public class TrackOperation {
 
   // @Pointcut("execution(* Operation.m*(..))")
-  // public void beforeafter() {
-  // }
-  // @Before("beforeafter()")
-  // @After("beforeafter()")
-  // public void myadvice(JoinPoint jp) {
-  // System.out.println("additional concern");
-  // System.out.println(jp.getSignature());
-  // }
-  // @Pointcut("execution(int Operation.*(..))")
-  // public void afterreturning() {
-  // }
-  // @AfterReturning(pointcut = "afterreturning()", returning = "result")
-  // public void myadvice(JoinPoint jp, Object result) {
-  // System.out.println("additional concern");
-  // System.out.println("Method Signature: " + jp.getSignature());
-  // System.out.println("Result in advice: " + result);
-  // System.out.println("end of after returning advice...");
-  // }
-  // @Pointcut("execution(* Operation.*(..))")
-  // public void around() {
-  // }
-  // @Around("around()")
-  // public Object myadvice(ProceedingJoinPoint pjp) throws Throwable {
-  // System.out.println("additional concern before actual method");
-  // Object o = pjp.proceed();
-  // System.out.println("additional concern after actual method");
-  // return o;
-  // }
-  @Pointcut("execution(* Operation.validate*(..))")
-  public void afterthrowing() {
-  }
-
-  @AfterThrowing(pointcut = "afterthrowing()", throwing = "error")
-  public void myadvice(JoinPoint jp, Throwable error) {
-	System.out.println("additional concern");
-	System.out.println("Method Signature: " + jp.getSignature());
-	System.out.println("Exception is: " + error);
-	System.out.println("end of after throwing advice...");
-  }
+    // public void beforeafter() {
+    // }
+    // @Before("beforeafter()")
+    // @After("beforeafter()")
+//    public void myadvice(JoinPoint jp) {
+//        System.out.println("additional concern");
+//        System.out.println(jp.getSignature());
+//    }
+    // @Pointcut("execution(int Operation.*(..))")
+    // public void afterreturning() {
+    // }
+    // @AfterReturning(pointcut = "afterreturning()", returning = "result")
+//    public void myadvice(JoinPoint jp, Object result) {
+//        System.out.println("additional concern");
+//        System.out.println("Method Signature: " + jp.getSignature());
+//        System.out.println("Result in advice: " + result);
+//        System.out.println("end of after returning advice...");
+//    }
+    // @Pointcut("execution(* Operation.*(..))")
+    // public void around() {
+    // }
+    // @Around("around()")
+//    public Object myadvice(ProceedingJoinPoint pjp) throws Throwable {
+//        System.out.println("additional concern before actual method");
+//        Object o = pjp.proceed();
+//        System.out.println("additional concern after actual method");
+//        return o;
+//    }
+//  @Pointcut("execution(* Operation.validate*(..))")
+//  public void afterthrowing() {
+//  }
+//  @AfterThrowing(pointcut = "afterthrowing()", throwing = "error")
+    public void myadvice(JoinPoint jp, Throwable error) {
+        System.out.println("additional concern");
+        System.out.println("Method Signature: " + jp.getSignature());
+        System.out.println("Exception is: " + error);
+        System.out.println("end of after throwing advice...");
+    }
 }
